0e7444c098023dcb7bcacf57a138f8da16bd7fd0
Piyush <piyushsingh5629@gmail.com>
2026-03-29 19:30:35 +0530

    Update

---
+++ three-proxy.js
@@ -0,0 +1,57 @@
+/* ════════════════════════════════════════════════════════════
+   THREE Proxy for MindAR
+   ════════════════════════════════════════════════════════════ */
+// @ts-nocheck
+const THREE = window.THREE;
+
+export default THREE;
+export const Scene = THREE.Scene;
+export const PerspectiveCamera = THREE.PerspectiveCamera;
+export const WebGLRenderer = THREE.WebGLRenderer;
+export const Color = THREE.Color;
+export const Fog = THREE.Fog;
+export const AmbientLight = THREE.AmbientLight;
+export const PointLight = THREE.PointLight;
+export const DirectionalLight = THREE.DirectionalLight;
+export const HemisphereLight = THREE.HemisphereLight;
+export const Group = THREE.Group;
+export const Object3D = THREE.Object3D;
+export const Mesh = THREE.Mesh;
+export const Points = THREE.Points;
+export const Line = THREE.Line;
+export const LineSegments = THREE.LineSegments;
+export const Vector2 = THREE.Vector2;
+export const Vector3 = THREE.Vector3;
+export const Euler = THREE.Euler;
+export const Quaternion = THREE.Quaternion;
+export const Matrix4 = THREE.Matrix4;
+export const Raycaster = THREE.Raycaster;
+export const Box3 = THREE.Box3;
+export const Sphere = THREE.Sphere;
+export const Plane = THREE.Plane;
+export const Frustum = THREE.Frustum;
+export const BufferGeometry = THREE.BufferGeometry;
+export const CylinderGeometry = THREE.CylinderGeometry;
+export const BoxGeometry = THREE.BoxGeometry;
+export const SphereGeometry = THREE.SphereGeometry;
+export const ConeGeometry = THREE.ConeGeometry;
+export const PlaneGeometry = THREE.PlaneGeometry;
+export const RingGeometry = THREE.RingGeometry;
+export const TorusGeometry = THREE.TorusGeometry;
+export const ExtrudeGeometry = THREE.ExtrudeGeometry;
+export const Shape = THREE.Shape;
+export const ShapeGeometry = THREE.ShapeGeometry;
+export const MeshBasicMaterial = THREE.MeshBasicMaterial;
+export const MeshStandardMaterial = THREE.MeshStandardMaterial;
+export const MeshPhongMaterial = THREE.MeshPhongMaterial;
+export const PointsMaterial = THREE.PointsMaterial;
+export const LineBasicMaterial = THREE.LineBasicMaterial;
+export const DoubleSide = THREE.DoubleSide;
+export const AdditiveBlending = THREE.AdditiveBlending;
+export const TextureLoader = THREE.TextureLoader;
+export const Clock = THREE.Clock;
+export const AudioContext = THREE.AudioContext;
+export const AudioListener = THREE.AudioListener;
+export const Audio = THREE.Audio;
+export const AudioLoader = THREE.AudioLoader;
+export const MathUtils = THREE.MathUtils;
